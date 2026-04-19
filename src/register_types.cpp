8829d009c7dd934309ae41c30a966b40c55eb4ae
Кип <dd@gmail.com>
2026-04-19 20:13:08 +0300

    lab2

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,11 +1,11 @@
 #include "register_types.h"
 
+#include "data_structure_lab.h"
+
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
 #include <godot_cpp/godot.hpp>
-
-#include "example_class.h"
 
 using namespace godot;
 
@@ -14,7 +14,7 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level)
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	GDREGISTER_CLASS(ExampleClass);
+	GDREGISTER_CLASS(DataStructureLab);
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
